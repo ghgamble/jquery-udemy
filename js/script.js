fc5772bd66ebfc8613449f04dd043323664633c3
Grace Gamble <grace.h.gamble@gmail.com>
2018-06-05 15:55:22 -0600

    Fading Animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(function() {
-  // jQuery goes here...
+      // jQuery goes here...
 
-  // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+      // Uncomment this line to fade out the red box on page load
+      $(".red-box").fadeTo(1000, 0.25);
+      $(".green-box").fadeTo(1000, 0.5);
+      $(".blue-box").fadeTo(1000, 0.75);
+
+      $(".blue-box").fadeToggle();
+      $(".blue-box").fadeToggle();
 });
