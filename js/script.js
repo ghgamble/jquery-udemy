a9428ba15deeff325fcb0696e88c364f6538d72c
Grace Gamble <grace.h.gamble@gmail.com>
2018-06-05 17:19:23 -0600

    Traversing HTML Doc

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
 $(function() {
-      // jQuery goes here...
-
-      // Uncomment this line to fade out the red box on page load
-      $(".red-box").fadeTo(1000, 0.25);
-      $(".green-box").fadeTo(1000, 0.5);
-      $(".blue-box").fadeTo(1000, 0.75);
-
-      $(".blue-box").fadeToggle();
-      $(".blue-box").fadeToggle();
+      $('#list').prev().css("background-color", "rgba(180, 180, 30, 0.8)");
 });
