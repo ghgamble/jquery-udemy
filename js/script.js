0a3f417b8ebdbfa1ae076307073bc08ad8b008b3
Grace Gamble <grace.h.gamble@gmail.com>
2018-06-05 17:34:28 -0600

    Finish Element Selectors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 $(function() {
-      $('#list').prev().css("background-color", "rgba(180, 180, 30, 0.8)");
+      // $('#list').find("li").filter(":even").css("background-color", "rgba(180, 180, 30, 0.8)");
+      $('li').filter(function(index){
+            return index % 3 === 1;
+      }).css("background-color", "rgba(180, 180, 30, 0.8)");
 });
